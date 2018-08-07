a51fc95568703ccc05719342d6a9ed1bb4e63f83
BenkiKuu <iganeleo@gmail.com>
2018-08-07 11:51:47 +0300

    creates the registration forms templates files and code for the registration file

--- nyumba/views.py
+++ nyumba/views.py
@@ -1,7 +1,8 @@
 from . models import NeighbourHood, Profile, Business
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required
 def neighbourhood(request):
     title = 'neighbourhoods'
     neighbourhoods = NeighbourHood.objects.all()
