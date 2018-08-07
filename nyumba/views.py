33bf2369d638b3aa105390d0b8eb3fd9c43c8180
BenkiKuu <iganeleo@gmail.com>
2018-08-07 11:17:12 +0300

    adds the index view

--- nyumba/views.py
+++ nyumba/views.py
@@ -1,3 +1,8 @@
+from . models import NeighbourHood, Profile, Business
 from django.shortcuts import render
 
 # Create your views here.
+def neighbourhood(request):
+    title = 'neighbourhoods'
+    neighbourhoods = NeighbourHood.objects.all()
+    return render(request, 'index.html', locals())
